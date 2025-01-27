be567b73a3002cfdaf4eae6a4cdbf031c19487fc
Nipun Jain <realnipunjain05@gmail.com>
2025-01-27 23:24:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,25 +23,9 @@ def predict_file(file: UploadFile = File(...)):
     contents = file.file.read()
     input_data = pd.read_csv(io.BytesIO(contents))
 
-    # Exclude the Potability column if it exists
-    if "Potability" in input_data.columns:
-        input_data = input_data.drop(columns=["Potability"])
-
-    # Define the expected columns
-    expected_columns = [
-        "ph", "Hardness", "Solids", "Chloramines",
-        "Sulfate", "Conductivity", "Organic_carbon",
-        "Trihalomethanes", "Turbidity"
-    ]
-
-    # Ensure only the required columns are used
-    input_data = input_data[expected_columns]
-
-    # Handle missing values (e.g., fill with the mean of each column)
-    input_data = input_data.fillna(input_data.mean())
-
     # Perform predictions
-    input_data["Potability or Not"] = model.predict(input_data).apply(lambda x: "Yes" if x == 1 else "No")
+    predictions = model.predict(input_data)
+    input_data['Potability or Not'] = ["Yes" if pred == 1 else "No" for pred in predictions]
 
     # Save the resulting DataFrame to a new CSV
     output_file_path = "output_with_predictions.csv"
